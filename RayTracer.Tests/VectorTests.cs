d5f8ee974592ba232da3b615c7b1959c8916309d
rifat <rifat@kth.se>
2025-12-15 22:19:01 +0100

    Add: Tests for Vector functions.

--- RayTracer.Tests/VectorTests.cs
+++ RayTracer.Tests/VectorTests.cs
@@ -8,8 +8,36 @@ public class VectorTests
     [Fact]
     public void Length()
     {
-        var v = new Vector(3, 4, 0);
+        Vector v = new Vector(3, 4, 0);
         Assert.Equal(5, v.Length());
+    }
+
+    [Fact]
+    public void Normalize()
+    {
+        Vector v = new Vector(1, 2, 3);
+        Vector unit = v.Normalize();
+        Assert.Equal(1, unit.Length());
+        
+        v = new Vector(-251, 1, 28);
+        unit = v.Normalize();
+        Assert.Equal(1, unit.Length());
+    }
+
+    [Fact]
+    public void DotProduct()
+    {
+        Vector v1 = new Vector(1, 2, 3);
+        Vector v2 = new Vector(3, 4, 5);
+        double dot = v1.DotProduct(v2);
+        
+        Assert.Equal(26, dot);
+        
+        v1 = new Vector(1, 2, -3);
+        v2 = new Vector(3, 4, 5);
+        dot = v1.DotProduct(v2);
+        Assert.Equal(-4, dot);
+
     }
     
 }

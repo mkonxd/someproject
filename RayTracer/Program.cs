f4a0d968751685dc6b5e9aa0b994f3a92ec05dda
rifat <rifat@kth.se>
2025-12-15 21:10:32 +0100

    Add: Shape Interface

--- RayTracer/Program.cs
+++ RayTracer/Program.cs
@@ -1,6 +1,14 @@
-namespace DefaultNamespace;
+﻿using System.Runtime.Intrinsics.Arm;
+using System.Security.AccessControl;
+
+namespace Raytracer;
+
+using System;
 
 public class Program
 {
-    
+    static void Main(string[] args)
+    {
+        
+    }
 }

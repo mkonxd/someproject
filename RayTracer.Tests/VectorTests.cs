1412bd006d5d7f898eedba0757669f0fa28d00aa
rifat <rifat@kth.se>
2025-12-15 22:28:54 +0100

    Add: More vector tests.

--- RayTracer.Tests/VectorTests.cs
+++ RayTracer.Tests/VectorTests.cs
@@ -39,5 +39,33 @@ public class VectorTests
         Assert.Equal(-4, dot);
 
     }
+
+    [Fact]
+    private void Operators()
+    {
+        Vector v1 = new Vector(1, 2, 3);
+        Vector v2 = v1 * 2;
+        Assert.Equal(2, v2.X);
+        Assert.Equal(4, v2.Y);
+        Assert.Equal(6, v2.Z);
+        
+        v1 = new Vector(1, 2, 3);
+        v2 = 2 * v1;
+        Assert.Equal(2, v2.X);
+        Assert.Equal(4, v2.Y);
+        Assert.Equal(6, v2.Z);
+        
+        v1 = new Vector(2, 3, 4);
+        v2 = new Vector(2, 3, -4);
+        Vector v3 = v1 + v2;
+        Assert.Equal(4, v3.X);
+        Assert.Equal(6, v3.Y);
+        Assert.Equal(0, v3.Z);
+
+        v3 = v1 - v2;
+        Assert.Equal(0, v3.X);
+        Assert.Equal(0, v3.Y);
+        Assert.Equal(8, v3.Z);
+    }
     
 }
